038c1a3ad8f2e5b0672a6f9396fd430f86199fff
Ritu00raj <160773612+Ritu00raj@users.noreply.github.com>
2024-07-16 11:51:17 +0530

    Create Lecture1.java

---
+++ Lecture 1 - CipherSchools/Lecture1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Lecture1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        System.out.println("X is = " + x);
+        System.out.println("Y is = " + y);
+        byte b = 127;
+        System.out.println("B is = " + b);
+        byte b2 = (byte) 129;
+        System.out.println("B2 is = " + b2);
+    }
+}
